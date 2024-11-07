67741e93a2d8cca8a7f923080d30d7ddf62b60a6
Osama Al-mamari <43526628+OsamaAlmamri@users.noreply.github.com>
2024-11-07 21:17:34 +0300

    another way to pass props

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -4,14 +4,14 @@ export type  SectionProp = {
 }
 
 
-function SectionTitle(props: SectionProp) {
+function SectionTitle({title, description}: SectionProp) {
 
     return (
         <div className="grid grid-cols-12 justify-content-center text-center">
             <div className=" col-span-12 md:col-span-10 lg:col-span-8  ">
                 <div className="header-section">
-                    <h2 className="title">{props.title}</h2>
-                    <p className="description">{props.description}</p>
+                    <h2 className="title">{title}</h2>
+                    <p className="description">{description}</p>
                 </div>
             </div>
         </div>
