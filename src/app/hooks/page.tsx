5e468b23b98587f58877692e74ddac7e9296c5e8
Osama Al-mamari <43526628+OsamaAlmamri@users.noreply.github.com>
2024-11-07 22:52:22 +0300

    useEffect

--- src/app/hooks/page.tsx
+++ src/app/hooks/page.tsx
@@ -1,23 +1,48 @@
 'use client';
 
 import HeaderNav from "@/components/HeaderNav/HeaderNav";
-import {useState} from "react";
-
+import {useEffect, useState} from "react";
 
 
 function Hooks() {
-    const [counter,setCounter] = useState<number>(0);
+    const [counter, setCounter] = useState<number>(0);
+    const [success, setSucess] = useState<boolean>(false);
 
     function chnageCounter() {
-        setCounter(counter+1) ;
+        setCounter(counter + 1);
+        if(counter==10)
+        {
+            setSucess(true);
+        }
     }
+
+    // useEffect(() => {
+    // console.log('this effect will call each time page change')
+    // });
+    //
+    // useEffect(() => {
+    // console.log('this effect will call one time ')
+    // },[]);
+    //
+    // useEffect(() => {
+    // console.log('this effect will call one counter  change ')
+    // },[counter]);
+
+    useEffect(() => {
+    console.log('this effect will call one counter ===10   ')
+    },[success]);
+
+    // useEffect(() => {
+    // console.log('this effect will call counter or success are change    ')
+    // },[success,counter]);
+
     return (
         <div className=" text-center items-center justify-center ">
             <HeaderNav/>
 
             <h1 className="text-center text-2xl"> {counter} </h1>
 
-            <button className="btn border bg-primary py-2 w-56 " onClick={chnageCounter}> increase </button>
+            <button className="btn border bg-primary py-2 w-56 " onClick={chnageCounter}> increase</button>
         </div>
     );
 }
